bdf461c4a9d722e4d29a6838e81f00a3c600a37e
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2021-10-31 22:55:16 -0700

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,3 @@
+weight=int(input())
+pounds=weight*2.2
+print("weight in kilograms:",pounds)
---
+++ sum_avg.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+c=int(input())
+sum=a+b+c
+print("sum of three numbers:",sum)
+avg=(sum)/3
+print("average of three numbers",avg)
