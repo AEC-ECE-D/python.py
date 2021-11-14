5269281886cb0d4d462330d0dd123b3e2b060b5e
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2021-11-14 15:30:47 +0530

    Add files via upload

---
+++ greater_numbers.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if (a>b)&(a>c):
+    print("A is the greatest")
+elif (b>a)&(b>c):
+    print("B is the greatest")
+else:
+    print("c is the greatest")
