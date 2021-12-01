c62c758977e9beef80159a19065794912230b4b9
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2021-12-01 17:24:16 +0530

    Add files via upload

---
+++ balanced_prime.py
@@ -0,0 +1,34 @@
+from math import *
+def isprime(num):
+    if num==1:
+        return False
+    sq=int(sqrt(num))
+    for i in range(2,sq+1):
+        if num%i==0:
+            return False
+    return True
+num=int(input())
+if isprime(num):
+    print("prime")
+    np=num+2
+    while True:
+        if isprime(np):
+            break
+        else:
+            np+=2
+    pp=num-2
+    while True:
+        if isprime(pp):
+            break
+        else:
+            pp-=2
+    if np*pp<=num*num:
+        print("good prime")
+    else:
+        prine("bad prime")
+    if np-num==num-pp:
+        print("balanced prime")
+    else:
+        print("un balanced prime")
+else:
+    print("not a prime")
