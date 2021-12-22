e3ddeddf61d83ae70a0e00f548ff96fbc6fa398f
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2021-12-22 20:53:48 +0530

    Add files via upload

---
+++ meal.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+
+def solve(meal_cost, tip_percent, tax_percent):
+    
+    tip=(meal_cost/100)*tip_percent
+    tax=(tax_percent/100)*tip_percent
+    total_cost=meal_cost+tip+tax
+    print(int(total_cost//1))
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
