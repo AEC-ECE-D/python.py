ec2a1508eaac9c79ffe1b1d66e70f2667896974f
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-01-21 11:36:11 +0530

    Add files via upload

---
+++ print_a_value_in_english.py
@@ -0,0 +1,24 @@
+n=int(input("enter a number from 0 to 9:"))
+if n==0:
+    print("zero")
+elif n==1:
+    print("one")
+elif n==2:
+    print("two")
+elif n==3:
+    print("three")
+elif n==4:
+    print("four")
+elif n==5:
+    print("five")
+elif n==6:
+    print("six")
+elif n==7:
+    print("seven")
+elif n==8:
+    print("eight")
+elif n==9:
+    print("nine")
+else:
+    print("please enter a number in between 0 to 9")
+    
