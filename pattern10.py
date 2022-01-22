5ab4632fe0d020737c4e6cc8cacb2ff98dfa6f6e
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-01-22 09:24:19 +0530

    Add files via upload

---
+++ pattern10.py
@@ -0,0 +1,15 @@
+n=int(input("enter no of rows:"))
+for i in range(n):
+    for j in range(n):
+        print(chr(65+i),end=" ")
+    print()
+"""
+enter no of rows:7
+A A A A A A A 
+B B B B B B B 
+C C C C C C C 
+D D D D D D D 
+E E E E E E E 
+F F F F F F F 
+G G G G G G G
+"""
---
+++ pattern8.py
@@ -0,0 +1,14 @@
+n=int(input("enter no. of rows:"))
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()
+"""
+enter no. of rows:6
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * *
+"""
