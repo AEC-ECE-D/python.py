ee7a6ee0f34542b2bb8cb2bb8ab19f419493a081
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-01-25 18:16:58 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,8 @@
+name="vivek"
+x=len(name)
+if x<3:
+    print("name must be atleast 3 characters")
+elif x>50:
+    print("name can be a maximum of 50 characters")
+else:
+    print("name looks good")
---
+++ largest_number_list.py
@@ -0,0 +1,3 @@
+numbers=[3,6,7,8,9,1]
+max=max(numbers)
+print(max)
