37d40639583bc2dd09514d6e62c3f2cb9ed2b4bc
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-02-02 11:52:19 +0530

    Add files via upload

---
+++ neon_number.py
@@ -0,0 +1,10 @@
+num = int(input("enter a number:"))
+sqr = num*num 
+sumOfDigit = 0
+while sqr>0:
+    sumOfDigit =sumOfDigit + sqr%10
+    sqr = sqr//10
+if (num == sumOfDigit):
+    print("Neon Number")
+else:
+    print("Not Neon Number")
